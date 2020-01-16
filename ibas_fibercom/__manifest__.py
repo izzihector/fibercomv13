dd3c51c752f1378af11fc7e1b5ecde001de4db02
Rein Duque <rey.duqueii@gmail.com>
2020-01-16 14:04:16 +0000

    Added MRF Option

--- ibas_fibercom/__manifest__.py
+++ ibas_fibercom/__manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "IBAS",
+    'website': "http://www.ibasuite.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,13 +20,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/ir_sequence_data.xml',
+        'views/sale_order.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ ibas_fibercom/models/sale.py
@@ -0,0 +1,45 @@
+
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo import _, api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+class IBASSale(models.Model):
+    _inherit = 'sale.order' 
+
+    ibas_order_type = fields.Selection([
+        ('MRF', 'MRF'),
+        ('SALE', 'Sale'),
+    ], string='Docment Type', default='MRF')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', _('New')) == _('New'):
+            seq_date = None
+            if 'date_order' in vals:
+                seq_date = fields.Datetime.context_timestamp(self, fields.Datetime.to_datetime(vals['date_order']))
+            if vals['ibas_order_type'] == "MRF":
+                if 'company_id' in vals:
+                    vals['name'] = self.env['ir.sequence'].with_context(force_company=vals['company_id']).next_by_code(
+                        'ibas_fibercom.mrf', sequence_date=seq_date) or _('New')
+                else:
+                    vals['name'] = self.env['ir.sequence'].next_by_code('ibas_fibercom.mrf', sequence_date=seq_date) or _('New')
+            else:
+                if 'company_id' in vals:
+                    vals['name'] = self.env['ir.sequence'].with_context(force_company=vals['company_id']).next_by_code(
+                        'sale.order', sequence_date=seq_date) or _('New')
+                else:
+                    vals['name'] = self.env['ir.sequence'].next_by_code('sale.order', sequence_date=seq_date) or _('New')
+
+        # Makes sure partner_invoice_id', 'partner_shipping_id' and 'pricelist_id' are defined
+        if any(f not in vals for f in ['partner_invoice_id', 'partner_shipping_id', 'pricelist_id']):
+            partner = self.env['res.partner'].browse(vals.get('partner_id'))
+            addr = partner.address_get(['delivery', 'invoice'])
+            vals['partner_invoice_id'] = vals.setdefault('partner_invoice_id', addr['invoice'])
+            vals['partner_shipping_id'] = vals.setdefault('partner_shipping_id', addr['delivery'])
+            vals['pricelist_id'] = vals.setdefault('pricelist_id', partner.property_product_pricelist and partner.property_product_pricelist.id)
+        result = super(IBASSale, self).create(vals)
+        return result
