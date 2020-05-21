d3caf45ef67f720ebbdba3f2c7ee6eba97ed53ba
Lawrence <jovenlawrencegersaniba.fcu@gmail.com>
2020-05-21 19:46:52 +0800

    scheduled date

--- jt_fcom_stock_ext/models/stock.py
+++ jt_fcom_stock_ext/models/stock.py
@@ -66,15 +66,12 @@ class StockMoveLine(models.Model):
         related='initial_demand', store=True, string='Demand')
 
     scheduled_date = fields.Datetime(
-        string='Scheduled Date', related='picking_id.scheduled_date')
-
-    scheduled_date_store = fields.Datetime(
-        string='Scheduled Dates', compute='_compute_scheduled_date', store=True)
+        string='Scheduled Date', compute='_compute_scheduled_date', store=True)
 
     @api.depends('picking_id')
     def _compute_scheduled_date(self):
         for rec in self:
             if self.picking_id:
                 self.update({
-                    'scheduled_date_store': rec.scheduled_date
+                    'scheduled_date': rec.picking_id.scheduled_date
                 })
